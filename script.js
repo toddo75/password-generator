180d2ee7f11ca71b0ea0fb95b02e8035a511cd5d
toddo75 <toddjsavard@gmail.com>
2023-08-24 16:06:17 -0400

    cleaned everything up with final touches, made certain everything functioned properly, added notes to css

--- script.js
+++ script.js
@@ -17,8 +17,13 @@ function generatePassword() {
   var confirmNumber = confirm("Would you like to use numbers? Click 'ok' for yes and 'cancel' for no."); 
  
   var confirmSpecial = confirm("Would you like to use spectial characters? Click 'ok' for yes and 'cancel' for no.");
-  
+
+  // prompt windo to ask length of password
   var userAnswer5 = prompt("How many characters would you like your password? Type a number between 8 and 128, type number and press 'ok'.");
+
+  // sets value of question of improper value is added by user
+  var userAnswer6 = true;
+
   
 // following 4 if statements to decipher which variable(s)/characters will be in the final password, console 'before' and 'after' were used
 // to see the character choices in the final superstring were correct, not needed but helps to see the function
@@ -46,17 +51,18 @@ function generatePassword() {
     console.log("after", superString);
   }
   
-  // following 2 if statements to make sure superstring length is 8 or more and 128 or less
+  // following 2 if statements to make sure superstring length is 8 or more and 128 or less if not when ok is clicked page reloads to reset the 
+  // character values
   if (userAnswer5 < 8) {
     alert("Your selection must be between 8 and 128! You must start over! Please hit 'ok'.");
-    if (confirmuseranswer5 === true)
-    return;
+    if (userAnswer6 === true)
+    location.reload();
     }
 
   if (userAnswer5 > 128) {
     alert("Your selection must be between 8 and 128! You must start over! Please hit 'ok'.");
-      if (confirmuseranswer5 === true)
-    return;
+      if (userAnswer6 === true)
+    location.reload();
     }
 
 // logs answer as to length of password
@@ -81,7 +87,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Adds event listener to write the password function
